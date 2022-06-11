1f17576d0ad113bd15d2be2e796bab0c2f69fc88
Gabriele <81332847+Gnifh01@users.noreply.github.com>
2022-06-11 10:40:50 +0200

    Update exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -19,7 +19,7 @@ class BankAccount {
   withdraw(amount) {
     try {
       if (amount < 0) {
-        throw new WithdrawNotPermittedError("The amount provided cannot be negative"); // Use custom Error class
+        throw new NegativeAmountError("The amount provided cannot be negative"); // Use custom Error class
       }
       if (this.#amount < amount) {
         throw new WithdrawNotPermittedError("You cannot withdraw more than account balance"); // Use custom Error class
